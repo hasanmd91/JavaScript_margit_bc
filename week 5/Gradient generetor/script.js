6d84e69f538453ef61b037ff8dcc07c0a8232e5c
hasanmd91 <hasanmd1991@hotmail.com>
2022-09-03 21:38:15 +0300

    has a bug

--- week 5/Gradient generetor/script.js
+++ week 5/Gradient generetor/script.js
@@ -1,25 +1,23 @@
 let inputFirst = document.getElementById("input1");
 let inputSecond = document.getElementById("input2");
 let buttons = document.querySelectorAll("label");
-let display = document.querySelector(".display");
-let color1 = "";
-let color2 = "";
+let display = document.querySelector(".container");
+let color1;
+let color2;
 let gradientPosition = "";
 
 inputFirst.addEventListener("input", () => {
   color1 = inputFirst.value;
-  console.log(color1);
 });
 
 inputSecond.addEventListener("input", () => {
   color2 = inputSecond.value;
-  console.log(color2);
 });
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     gradientPosition = button.id;
-    display.style.backgroundImage = `linear-gradient( ${gradientPosition}, ${color1},${color2})`;
-    console.log(gradientPosition, color1, color2);
+    display.style.backgorundImage = `linear-gradient(${gradientPosition}, ${color1}, ${color2})`;
+    console.log(`linear-gradient(${gradientPosition}, ${color1}, ${color2})`);
   });
 });
