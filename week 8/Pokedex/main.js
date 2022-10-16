f92b71944ad6c7750ef81656f10336fbd8d4e500
hasanmd91 <hasanmd1991@hotmail.com>
2022-10-16 23:45:10 +0300

    fix: switch function delete

--- week 8/Pokedex/main.js
+++ week 8/Pokedex/main.js
@@ -15,48 +15,7 @@ let generations = [
   { limit: 96, offset: 809 },
   { limit: 3, offset: 905 },
 ];
-const getIcons = (type) => {
-  switch (type) {
-    case "fire":
-      return "https://upload.wikimedia.org/wikipedia/commons/5/56/Pok%C3%A9mon_Fire_Type_Icon.svg";
-    case "flying":
-      return "https://upload.wikimedia.org/wikipedia/commons/e/e0/Pok%C3%A9mon_Flying_Type_Icon.svg";
-    case "ground":
-      return "https://upload.wikimedia.org/wikipedia/commons/8/8d/Pok%C3%A9mon_Ground_Type_Icon.svg";
-    case "ice":
-      return "https://upload.wikimedia.org/wikipedia/commons/8/88/Pok%C3%A9mon_Ice_Type_Icon.svg";
-    case "normal":
-      return "https://upload.wikimedia.org/wikipedia/commons/a/aa/Pok%C3%A9mon_Normal_Type_Icon.svg";
-    case "poison":
-      return "https://upload.wikimedia.org/wikipedia/commons/c/c4/Pok%C3%A9mon_Poison_Type_Icon.svg";
-    case "psychic":
-      return "https://upload.wikimedia.org/wikipedia/commons/a/ab/Pok%C3%A9mon_Psychic_Type_Icon.svg";
-    case "rock":
-      return "https://upload.wikimedia.org/wikipedia/commons/b/bb/Pok%C3%A9mon_Rock_Type_Icon.svg";
-    case "steel":
-      return "https://upload.wikimedia.org/wikipedia/commons/3/38/Pok%C3%A9mon_Steel_Type_Icon.svg";
-    case "water":
-      return "https://upload.wikimedia.org/wikipedia/commons/0/0b/Pok%C3%A9mon_Water_Type_Icon.svg";
-    case "bug":
-      return "https://upload.wikimedia.org/wikipedia/commons/3/3c/Pok%C3%A9mon_Bug_Type_Icon.svg";
-    case "dark":
-      return "https://upload.wikimedia.org/wikipedia/commons/0/09/Pok%C3%A9mon_Dark_Type_Icon.svg";
-    case "ghost":
-      return "https://upload.wikimedia.org/wikipedia/commons/a/a0/Pok%C3%A9mon_Ghost_Type_Icon.svg";
-    case "grass":
-      return "https://upload.wikimedia.org/wikipedia/commons/f/f6/Pok%C3%A9mon_Grass_Type_Icon.svg";
-    case "dragon":
-      return "https://upload.wikimedia.org/wikipedia/commons/a/a6/Pok%C3%A9mon_Dragon_Type_Icon.svg";
-    case "electric":
-      return "https://upload.wikimedia.org/wikipedia/commons/a/a9/Pok%C3%A9mon_Electric_Type_Icon.svg";
-    case "fairy":
-      return "https://upload.wikimedia.org/wikipedia/commons/0/08/Pok%C3%A9mon_Fairy_Type_Icon.svg";
-    case "fighting":
-      return "https://upload.wikimedia.org/wikipedia/commons/b/be/Pok%C3%A9mon_Fighting_Type_Icon.svg";
-    default:
-      return "#";
-  }
-};
+
 search.addEventListener("keyup", (e) => {
   let input = e.target.value.toLowerCase();
   const pokeCards = document.querySelectorAll(".card");
@@ -84,7 +43,10 @@ const displayPokemon = (pokemonss) => {
         <div>${data.types
           .map(
             (type) =>
-              '<img src="' + getIcons(type.type.name) + '" class="icons"/>'
+              '<img src="./icons/' +
+              type.type.name +
+              ".svg" +
+              '" class="icons"/>'
           )
           .join("")} </div>
       </div>
